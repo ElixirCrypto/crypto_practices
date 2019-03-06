24a17d04388626357ceffd2db77138ecdbd6d798
Sun <huachuang20@gmail.com>
2019-03-06 15:09:50 +0800

    fix mix info

--- mix.exs
+++ mix.exs
@@ -4,10 +4,21 @@ defmodule CryptoPractices.MixProject do
   def project do
     [
       app: :crypto_practices,
+      description: "Common cryptography algorithm and cryptanalysis.",
+      package: package(),
       version: "0.0.1",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
+    ]
+  end
+
+  defp package() do
+    [
+      name: "crypto_practices",
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE* CHANGELOG*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/ElixirCrypto/crypto_practices"}
     ]
   end
 
@@ -21,6 +32,7 @@ defmodule CryptoPractices.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
